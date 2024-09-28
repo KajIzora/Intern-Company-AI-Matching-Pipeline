80b8b28b584dc4a7208be932ca2dcdc2eea88ac4
KajIzora <KajHizora@gmail.com>
2024-09-28 16:48:32 -0700

    Add files via upload

---
+++ script_matching_code_anonymized/mypackage/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='2.3',
+    packages=find_packages(),
+    description='Useful Stuff',
+    author='Kaj',
+)
