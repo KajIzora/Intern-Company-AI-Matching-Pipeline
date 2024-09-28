2126d54843aadb9603b9e699610de6d282c6742d
KajIzora <KajHizora@gmail.com>
2024-09-28 16:46:29 -0700

    Delete script_matching_code_anonymized directory

--- script_matching_code_anonymized/mypackage/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='mypackage',
-    version='2.3',
-    packages=find_packages(),
-    description='Useful Stuff',
-    author='Kaj',
-)
